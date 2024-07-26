3f89bea597f2a5b99b6de73bc15f17c2d54793d7
Katrina Chen <katc7624@gmail.com>
2024-07-26 16:01:53 -0700

    add scroll feasture in team page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,15 +1,25 @@
 
 import data from "../data/team.json";
+import { Link } from "react-scroll";
 
 export default function Team(){
     return (
     <>
         <div className="team-container">
            <img src='/images/Anna.jpg' alt='Anna' />
-           <h>Meet Our Teams</h>
+           
+           <Link className="meet--team"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="teamsSection"
+           >
+            <h>Meet Our Teams</h>
+           </Link>
            <p>Disney Adults</p>
         </div>
-            <section className="teams--section" >
+            <section id="teamsSection" className="teams--section" >
             <div className="teams--section--container">
                 {data?.teams?.map((item, index) => (
                     <div key={index} className="teams--section--card">
