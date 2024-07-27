081028bb4b9c39ee4a2a212bd91d948e20f8890a
Katrina Chen <katc7624@gmail.com>
2024-07-26 21:13:29 -0700

    set up arrow button for rover

--- src/Pages/ControlCenter/Control.jsx
+++ src/Pages/ControlCenter/Control.jsx
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 import React, { useEffect, useState } from 'react';
-
+import {Link} from 'react-scroll'
 
 
 const socket = io('http://localhost:8000'); 
@@ -24,20 +24,97 @@ export default function ControlCenter(){
         socket.emit('example_topic', message);
       };
 
+
+      //arrow handle start
+
+      const roverUp=()=>{
+        socket.emit('send-direction','forward');
+      }
+      const roverDown=()=>{
+        socket.emit('send-direction','backward');
+      }
+      const roverLeft=()=>{
+        socket.emit('send-direction','left');
+      }
+      const roverRight=()=>{
+        socket.emit('send-direction','right');
+      }
+      const roverStop=()=>{
+        socket.emit('send-direction','stop');
+      }
+      //arrow handle end
+
+
     return(
         <>
-            {/* <div className="control--container">
+            <div className="control--container">
                 <video src="/videos/comm.mp4" autoPlay loop muted></video>
-            </div> */}
-            <div className="control--content">
-                <p>here</p>
+            
+              <div className="control--main--container">
+                <div className="data-display-box">
+                  <h>Live Data On Mars:</h>
+                  <p>Temperatur: {data.temp}</p>
+                  <p>Humidity: {data.humidity}</p>
+                </div>
+                <div className="control--robotics--container">
+        
+                  <p style={{color:'#8e93d4',fontSize:'25px'}}>Control Options</p>
+                  <Link 
+                  className="rover"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  to="Rover"
+                   >
+                    <i style={{color:'#ffffff', fontSize: '30px',marginRight:'50px',marginLeft:'20px'}} class="fa-solid fa-car"></i>
+                    <span style={{color:'#849dab',fontSize:'30px',fontFamily:'fantasy'}}>
+                      Rover
+                    </span>
+                  </Link>
+
+                  <Link className="arm"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  to="Rover"
+                  
+                  >
+                    <i style={{color:'#ffffff', fontSize: '30px',marginRight:'50px'}} class="fa-solid fa-dharmachakra"></i>
+                    <span style={{color:'#849dab',fontSize:'30px',fontFamily:'fantasy'}}>
+                      Arm
+                    </span>
+                  </Link>
+                  
+                </div>
+              </div>
             </div>
-            <div>
-                <p>Temperatur: {data.temp}</p>
-                <p>Humidity: {data.humidity}</p>
-                <button onClick={() => exampleSend('Hello, Server!')}>Send Message</button>
+
+            <div id="Rover">
+              <p style={{color:'#ffffff'}}>Rover</p>
+              
+              <div className="arrow--container">
+                <div className='rover--firstrow'>
+                  <a onClick={roverUp()} className="arrow">⬆️</a>
+                </div>
+                <div>
+                  <a onClick={roverLeft()} className="arrow">⬅️</a>
+                  <a onClick={roverStop()} className="circle">🔵</a>
+                  <a onClick={roverRight()} className="arrow">➡️</a>
+                </div>
+                <div className='rover--thirdrow'>
+                  <a onClick={roverDown()} className="arrow">⬇️</a>
+                </div>
+              </div>
                
             </div>
+            {/* 
+            <div>
+                
+                <button onClick={() => exampleSend('Hello, Server!')}>Send Message</button>
+               
+            </div> */}
         </>
 
     )
