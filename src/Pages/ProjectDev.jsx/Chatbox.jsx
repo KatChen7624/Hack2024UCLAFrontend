04f1bacb72fde3ac42b17cd2f83c262fcfd0656a
Katrina Chen <katc7624@gmail.com>
2024-07-29 10:51:49 -0700

    set up template for sidebar || and iframe for google doc

--- src/Pages/ProjectDev.jsx/Chatbox.jsx
+++ src/Pages/ProjectDev.jsx/Chatbox.jsx
@@ -16,10 +16,41 @@ function Chatbox() {
                 <img src="/images/chatbox.jpg"/>
             </div>
         </div>
+        
+       
+
     </div>
-    <div>
-        placeholder
+
+    <div className='dev--web--title--container'>
+        <h1>Website Section</h1>
     </div>
+     <div id="webSection" style={{ height: '100vh', overflow: 'auto' }}>
+        <iframe
+            src="https://docs.google.com/document/d/1gtvX5dtuveY15gI_K-3lep4ZqNut2jupBJs6-tUvzzo/edit?usp=sharing"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allowFullScreen
+            title="Google Doc"
+        ></iframe>
+    </div>
+
+    <div className='dev--web--title--container'>
+        <h1>Pico&&Circuit Section</h1>
+    </div>
+     <div id="circuitSection" style={{ height: '100vh', overflow: 'auto' }}>
+        <img
+            src="/images/Anna.jpg"
+            width="100%"
+            alt="not found"
+        ></img>
+    </div>
+
+    
+
+   
+
+
     </>
   )
 }
